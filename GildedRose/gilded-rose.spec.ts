8ca5b662bd51e4c1882b4b207314d721fcd3ac1e
Rob Berry <rob@robb.re>
2024-01-30 15:14:02 +0000

    Add some tests to capture base functionality
    First phase of refactoring - handle unknown items

--- GildedRose/gilded-rose.spec.ts
+++ GildedRose/gilded-rose.spec.ts
@@ -1,9 +1,45 @@
 import { Item, GildedRose } from './gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it("should not allow negative quality", () =>{
+    const gildedRose = new GildedRose([new Item('foo', 0, -1)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+    expect(items[0].quality).toBe(0);
+  })
+
+  it("should constrain quality to a max of 50 ", () =>{
+    const gildedRose = new GildedRose([new Item('foo', 0, 52)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  })
+
+  it("should decrease the quality of an unknown item", () =>{
+    const gildedRose = new GildedRose([new Item('unknown item a', 2, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(5);
+  })
+
+  it("should decrease the sellin of an unknown item", () =>{
+    const gildedRose = new GildedRose([new Item('unknown item a', 2, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(1);
+  })
+
+  it("should maintain the quality of sulfuras", () =>{
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 2, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(4);
+  })
+
+  it("should increase the quality of aged brie", () =>{
+    const gildedRose = new GildedRose([new Item('Aged Brie', 2, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(5);
+  })
+
+  it("should decrease the sellIn of aged brie", () =>{
+    const gildedRose = new GildedRose([new Item('Aged Brie', 2, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(1);
+  })
 });
--- GildedRose/gilded-rose.ts
+++ GildedRose/gilded-rose.ts
@@ -3,10 +3,43 @@ export class Item {
   sellIn: number;
   quality: number;
 
-  constructor(name:string, sellIn:number, quality:number) {
+  constructor(name: string, sellIn: number, quality: number) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
+  }
+}
+
+const increaseQuality = (item: Item) => {
+  return item.quality += 1
+}
+
+
+const maintainQuality = (item : Item) => {
+  return item.quality
+}
+
+
+type SUPPORTED = Record<string, (a: any) => any>
+
+const SUPPORTED_ITEMS: SUPPORTED = {
+  "Aged Brie": increaseQuality,
+  "Backstage passes to a TAFKAL80ETC concert": increaseQuality,
+  "Sulfuras, Hand of Ragnaros": maintainQuality
+}
+
+const defaultQuality = (item: Item) => {
+  return item.quality - 1
+}
+
+const qualityOfUnknownItem = (item: Item): undefined | number => {
+
+  let fn = SUPPORTED_ITEMS[item.name]
+
+  if (typeof (fn) === "undefined") {
+    return defaultQuality(item)
+  } else {
+    return fn(item)
   }
 }
 
@@ -19,12 +52,15 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
+
+      let item = this.items[i]
+      let quality = qualityOfUnknownItem(item)
+
+      if (typeof (quality) === "number") {
+        item.quality = quality
+
+        // FIXME: use item everywhere
+        this.items[i] = item
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
@@ -42,6 +78,7 @@ export class GildedRose {
           }
         }
       }
+
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
